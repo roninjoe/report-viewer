077d88d7d6e44ff9f102283cb957d7e6e59eeb66
Joe Ronin <joe@Joes-Air.home>
2020-04-16 16:51:47 -0400

    add initial session

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const session = require('express-session');
 const mysql = require('mysql');
 
 console.log("Starting report-viewer::main");
 
 const app = express();
+app.use(session({
+    secret : "putSecretHere", 
+    resave : false,
+    saveUninitialized : false}));
+
 app.use(express.static('./public'));
 
 
@@ -11,7 +17,14 @@ app.use(express.static('./public'));
 //     res.send("hit route /");
 // });
 app.get('/api', (req, res) => {
-    res.send("hit route /api");
+    if (req.session.page_views) {
+        res.send("visit " + req.session.page_views++);
+    } else {
+        req.session.page_views = 1;
+        res.send("first visti to route /api");
+    }
+    
+
 });
 
 app.listen('3000', () => {
