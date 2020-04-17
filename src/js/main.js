aa330dca7bdc4b1418c2c665b7b4c7af2074ebb7
Joe Ronin <joe@Joes-Air.home>
2020-04-17 16:59:02 -0400

    initial connect to db

--- src/js/main.js
+++ src/js/main.js
@@ -15,24 +15,49 @@ if (fs.existsSync(configPath)) {
     return;
 }
 
+// Create an Express server.
 const app = express();
+// Enable sessions
 app.use(session({
     secret : config.sessionSalt, 
     resave : false,
     saveUninitialized : false}));
 
+ // Serve up the ui as static files   
 app.use(express.static('./public'));
 
+// Create a database connection
+const db = mysql.createConnection({
+    host     : config.dbHost,
+    user     : config.dbUser,
+    password : config.dbPassword,
+    database : config.dbName
+});
 
-// app.get('/', (req, res) => {
-//     res.send("hit route /");
-// });
+// Connect
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySql Connected...');
+});
+
+// Query 
 app.get('/api', (req, res) => {
+    let sql = 'select * from report_viewer.report';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    });
+});
+
+app.get('/sessionCount', (req, res) => {
     if (req.session.page_views) {
         res.send("visit " + req.session.page_views++);
     } else {
         req.session.page_views = 1;
-        res.send("first visti to route /api");
+        res.send("first visit");
     }
     
 
