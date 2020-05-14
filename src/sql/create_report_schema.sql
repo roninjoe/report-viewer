9291656fe26403de102d7463dddd3788aa6d1b6d
joe <3817306+roninjoe@users.noreply.github.com>
2020-05-14 13:51:55 -0400

    create initial user

--- src/sql/create_report_schema.sql
+++ src/sql/create_report_schema.sql
@@ -3,6 +3,12 @@ drop database if exists report_results;
 create database report_viewer;
 create database report_results;
 
+-- Create 'report' user.  Will be forced to update password later.
+create user if not exists 'report'@'localhost' identified by 'report';
+
+grant all privileges on report_viewer.* to 'report'@'localhost';
+grant all privileges on report_results.* to 'report'@'localhost';
+
 CREATE TABLE report_viewer.report (
   id int(11) NOT NULL AUTO_INCREMENT,
   name varchar(200) NOT NULL,
