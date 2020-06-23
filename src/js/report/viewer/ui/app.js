54ae0eb7bcbfaf1bf9aa63ee4aeb6799399acd46
joe <3817306+roninjoe@users.noreply.github.com>
2020-06-23 11:00:24 -0400

    some initial (angular js for now) routing

--- src/js/report/viewer/ui/app.js
+++ src/js/report/viewer/ui/app.js
@@ -1,8 +1,20 @@
 var app = angular.module('app', ['ngRoute']);
-// angular.module('app',[]).controller('HelloWorldCtrl',
 
+app.config(function($routeProvider) {
+  console.log("Entering app.config")
+  $routeProvider
+  .when("/", {
+  templateUrl : "ui/report_list.html",
+  controller : "reportListCtrl"
+  })
+  .when("/report/:name", {
+    templateUrl : "ui/report.html"
+    })
+});  
+
+// Declaring inline temporarily.  (separate file fails)
 app.controller("reportListCtrl",
 function($scope) {
-    $scope.message = "This is the reportListCtrl message"
+    $scope.message = "message from reportListCtrl in app.js"
   }
-)
+);
