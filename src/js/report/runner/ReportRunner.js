5d0d8ead7b868e9db1355be1ca4d6ac0a2f7fd7e
joe <3817306+roninjoe@users.noreply.github.com>
2020-06-25 17:17:22 -0400

    initial sample reports dir

--- src/js/report/runner/ReportRunner.js
+++ src/js/report/runner/ReportRunner.js
@@ -1,6 +1,7 @@
 console.log("Entering ReportRunner")
 if (process.argv.length > 2) {
     console.log(`running report ${process.argv.slice(2)}`);
+    var config = App.loadConfig("config/server-config.json");
 }
  module.exports = class ReportRunner {
 
--- src/js/report/util/App.js
+++ src/js/report/util/App.js
@@ -1,9 +1,19 @@
-
+const fs = require('fs');
  module.exports = class App {
 
     constructor(){
         console.log("Entering App::constructor");
     }
 
-
+    static loadConfig(configPath) {
+        console.log("App::loadConfig");
+        if (fs.existsSync(configPath)) {
+            var config = JSON.parse(fs.readFileSync(configPath,'utf8'));
+            return config;
+            console.log("loaded config");
+        } else {
+            console.log(`ERROR: config not found ${configPath}`);
+            throw "config was not found";
+        }
+    }
 }
